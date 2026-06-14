778013209aa71a03ae552d46ab3dc3cca3fe14c4
Drull2000 <vladskalozub@icloud.com>
2026-06-14 11:10:23 +0300

    added third task

--- Practise/14.06.26/third.js
+++ Practise/14.06.26/third.js
@@ -1,3 +1,39 @@
-class pidor {
-    
+class ExtendedArray extends Array {
+
+    getString(separator = ", ") {
+        return this.join(separator);
+    }
+
+    getHtml(tagName) {
+
+        if (tagName.toLowerCase() === "li") {
+            let html = "<ul>\n";
+
+            this.forEach(item => {
+                html +=   <li>${item}</li>\n;
+            });
+
+            html += "</ul>";
+            return html;
+        }
+
+        return this.map(item => `<${tagName}>${item}</${tagName}>`).join("\n");
+    }
 }
+
+const arr = new ExtendedArray(
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React"
+);
+
+console.log(arr.getString(", "));
+console.log(arr.getString(" - "));
+console.log(arr.getString(" "));
+
+console.log("\nHTML з тегом p:");
+console.log(arr.getHtml("p"));
+
+console.log("\nHTML з тегом li:");
+console.log(arr.getHtml("li"));
